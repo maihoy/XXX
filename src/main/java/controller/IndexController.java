d04639ea7aad0a082ba6bfbdfb3f85eefc11e928
maihoy <maihoy494@gmail.com>
2016-03-06 21:53:27 +0300

    7th?

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -2,22 +2,23 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import model.Pizza;
+
 @Controller
-@RequestMapping("/")
 
 public class IndexController {
-    @RequestMapping(method = RequestMethod.GET)
-    public String sayHello(ModelMap model) {
-        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
-        return "welcome";
-    }
 
-    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
-    public String sayHelloAgain(ModelMap model) {
-        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
-        return "welcome";
+    @RequestMapping(value="/pizzavalley/{pizzaName}", method = RequestMethod.GET)
+    public String getPizza(@PathVariable String pizzaName, ModelMap model) {
+
+        Pizza pizza = new Pizza(pizzaName);
+        model.addAttribute("pizza", pizza);
+
+        return "pizza";
+
     }
 }
