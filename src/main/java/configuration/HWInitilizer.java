215fb565656951e0ebe004c7ec9862ad5e8a1ecf
maihoy <maihoy494@gmail.com>
2016-03-09 21:58:28 +0300

    10th

--- src/main/java/configuration/HWInitilizer.java
+++ src/main/java/configuration/HWInitilizer.java
@@ -1,26 +1,22 @@
 package configuration;
 
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+public class HWInitilizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { HWConfiguration.class };
+    }
 
-public class HWInitilizer implements WebApplicationInitializer {
-    public void onStartup(ServletContext container) throws ServletException {
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
 
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.register(HWConfiguration.class);
-        ctx.setServletContext(container);
-
-        ServletRegistration.Dynamic servlet = container.addServlet(
-                "dispatcher", new DispatcherServlet(ctx));
-
-        servlet.setLoadOnStartup(1);
-        servlet.addMapping("/");
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
     }
 
 }
