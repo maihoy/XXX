7918f5e96740ebfae0c59293c191231c4103645a
maihoy <maihoy494@gmail.com>
2016-03-15 12:21:44 +0300

    14th!

--- src/main/java/com/qwerty/controller/IndexController.java
+++ src/main/java/com/qwerty/controller/IndexController.java
@@ -1,13 +1,13 @@
-package controller;
+package com.qwerty.controller;
 
-import domain.Message;
+import com.qwerty.domain.Message;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-  @RequestMapping("/hello/{player}")
+  @RequestMapping("hello/{player}")
     public Message message(@PathVariable String player){
       Message msg = new Message(player, "Hello"+player);
       return msg;
