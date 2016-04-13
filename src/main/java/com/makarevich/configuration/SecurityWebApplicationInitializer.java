8c867dcaa73ecb759264f7e5fdc5a0c89cc58a30
maihoy <maihoy494@gmail.com>
2016-04-13 14:05:48 +0300

    ijn

---
+++ src/main/java/com/makarevich/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.makarevich.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by j on 13.4.16.
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
